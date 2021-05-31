175359968350b64c7175ae6d775dc6afa0e3ba72
vanlek <vanrezkysadewa1@gmail.com>
2021-06-01 01:12:44 +0700

    optimasi google maps pada mobile browser part 1

--- application/views/home/v_home_map.php
+++ application/views/home/v_home_map.php
@@ -83,19 +83,10 @@
             icon: {
                 url: 'https://gis.test/gis-tpa/assets/img/icon/me.png'
             },
-            animation: google.maps.Animation.BOUNCE
+            animation: google.maps.Animation.BOUNCE,
+            optimized: false
         }
         createMarker(location);
-    }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation ?
-            "Error: The Geolocation service failed." :
-            "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
     }
 
     // klik peta
